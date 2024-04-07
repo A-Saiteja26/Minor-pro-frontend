69f43c3ebd6b04ce7374c742458cc23d8917ccd6
A-Saiteja-26 <annamsaiteja4032@gmail.com>
2024-04-07 12:27:03 +0530

    package

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
  export default defineConfig({
   plugins: [react()],
   server: {
-    host:true,
-    port:80,
+
+    
     proxy: {
 
       '/myapp': {
@@ -15,5 +15,7 @@ import react from '@vitejs/plugin-react'
       }
       
     }
-  }
+  },
+
+
 })
